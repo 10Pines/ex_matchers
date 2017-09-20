c2b59f9700718cf4768313e74fd96b3840343f84
Erwin Debusschere <erwincdl@gmail.com>
2017-09-20 10:18:06 -0300

    Added matching macro
    This will help avoid logic repetition

---
+++ lib/ex_matchers/helpers.ex
@@ -0,0 +1,18 @@
+defmodule ExMatchers.Helpers do
+    
+    defmacro match_logic(actual, expected, do: logic) do
+        quote do
+            def to_match(actual_value, expected_value) do
+                var!(unquote(actual)) = actual_value
+                var!(unquote(expected)) = expected_value
+                assert unquote(logic)
+            end
+
+            def to_not_match(actual_value, expected_value) do
+                var!(unquote(actual)) = actual_value
+                var!(unquote(expected)) = expected_value
+                refute unquote(logic)
+            end
+        end
+    end
+end
