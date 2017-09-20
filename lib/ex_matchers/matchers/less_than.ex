deb105c556d4c00a1184d64a7078525f9b8f3459
Erwin Debusschere <erwincdl@gmail.com>
2017-09-20 11:43:53 -0300

    Refactored matchers

--- lib/ex_matchers/matchers/less_than.ex
+++ lib/ex_matchers/matchers/less_than.ex
@@ -3,28 +3,26 @@ defmodule ExMatchers.LessThan do
 
   import ExUnit.Assertions
   import ExMatchers.Custom
+  import ExMatchers.Helpers
 
   defprotocol LessThanMatcher do
     @fallback_to_any true
+
     def to_match(actual, another)
     def to_not_match(actual, another)
   end
 
   defimpl LessThanMatcher, for: [Integer, Float] do
-    def to_match(actual, another) do
-      assert actual < another
-    end
-    def to_not_match(actual, another) do
-      refute actual < another
+    
+    match_logic(actual, expected) do
+      actual < expected  
     end
   end
 
   defimpl LessThanMatcher, for: Any do
-    def to_match(actual, another) do
-      flunk "Less than not supported between #{actual} and #{another}"
-    end
-    def to_not_match(actual, another) do
-      flunk "Less than not supported between #{actual} and #{another}"
+
+    unsupported(actual, expected) do
+      "Less than not supported between #{actual} and #{expected}"
     end
   end
 
--- lib/ex_matchers/matchers/raise_error.ex
+++ lib/ex_matchers/matchers/raise_error.ex
@@ -5,15 +5,19 @@ defmodule ExMatchers.RaiseError do
   import ExMatchers.Custom
 
   defmodule RaiseErrorMatcher do
+    
     def to_match(function, error) do
       assert_raise error, function
     end
+
     def to_match(function, error, message) do
       assert_raise error, message, function
     end
+
     def to_not_match(function, error) do
       flunk "Matcher does not support to not match an error not being raised on #{function} with #{error}"
     end
+
     def to_not_match(function, error, message) do
       flunk "Matcher does not support to not match an error not being raised on #{function} with #{error} and #{message}"
     end
