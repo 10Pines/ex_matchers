4f81616415015f22ccf23b4eb909bc0382cfc166
Erwin Debusschere <erwincdl@gmail.com>
2017-09-20 11:24:24 -0300

    Added unsupported helper

--- lib/ex_matchers/helpers.ex
+++ lib/ex_matchers/helpers.ex
@@ -15,4 +15,20 @@ defmodule ExMatchers.Helpers do
             end
         end
     end
+
+    defmacro unsupported(actual, expected, do: logic) do
+        quote do
+            def to_match(actual_value, expected_value) do
+                var!(unquote(actual)) = actual_value
+                var!(unquote(expected)) = expected_value
+                flunk unquote(logic)
+            end
+
+            def to_not_match(actual_value, expected_value) do
+                var!(unquote(actual)) = actual_value
+                var!(unquote(expected)) = expected_value
+                flunk unquote(logic)
+            end
+        end
+    end
 end
